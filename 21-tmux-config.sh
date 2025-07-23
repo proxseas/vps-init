a89c06f1b7a2d4105cfd4e88df6d095fcaaa3bfa
username <a@b.com>
2025-07-22 21:36:46 -0700

    Enhance tmux configuration in `21-tmux-config.sh`
    - Updated default shell to zsh and set terminal type to xterm-256color.
    - Added key bindings for mouse support and Vim-like pane navigation.
    - Implemented new shortcuts for splitting panes and switching between them without prefix.
    - Increased history limit for tmux sessions to 10,000 entries.
    - Improved config reloading message for better user feedback.

--- 21-tmux-config.sh
+++ 21-tmux-config.sh
@@ -27,32 +27,40 @@ fi
 # Add basic tmux configuration if file is empty or doesn't exist
 if [[ ! -f "$TMUX_CONF" ]] || [[ ! -s "$TMUX_CONF" ]]; then
     cat > "$TMUX_CONF" <<'EOF'
-# Set zsh as default shell
+# Set zsh as default shell and terminal to xterm-256color
 set-option -g default-shell /usr/bin/zsh
-
-# Enable mouse support
-set -g mouse on
-
-# Set prefix to Ctrl-a
-set -g prefix C-a
-unbind C-b
-bind C-a send-prefix
-
-# Split panes using | and -
-bind | split-window -h
-bind - split-window -v
-unbind '"'
-unbind %
+set -g default-terminal "xterm-256color"
 
 # Reload config file
-bind r source-file ~/.tmux.conf \; display-message "Config reloaded!"
+bind r source-file ~/.tmux.conf \; display-message "TMUX Config reloaded!"
 
-# Don't rename windows automatically
-set-option -g allow-rename off
+## Set the displaying of seconds
+set -g status-right "%H:%M:%S"
+set -g status-interval 1
 
-# Start window numbering at 1
-set -g base-index 1
-set -g pane-base-index 1
+set -g history-limit 10000
+
+# switch panes using Alt-arrow without prefix
+bind -n M-Left select-pane -L
+bind -n M-Right select-pane -R
+bind -n M-Up select-pane -U
+bind -n M-Down select-pane -D
+
+## Vim-like pane navigation
+bind -n 'M-h' select-pane -L
+bind -n 'M-j' select-pane -D
+bind -n 'M-k' select-pane -U
+bind -n 'M-l' select-pane -R
+
+## Split panes using Ctrl-Alt-arrow without prefix
+bind -n 'C-M-h' split-window -h
+bind -n 'C-M-l' split-window -h
+bind -n 'C-M-j' split-window -v
+bind -n 'C-M-k' split-window -v
+
+# Enable mouse support
+# set -g mouse on
+
 EOF
 fi
 
